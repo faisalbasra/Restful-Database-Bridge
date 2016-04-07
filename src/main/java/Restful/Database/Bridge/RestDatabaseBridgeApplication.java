7a497c0c7150c02e3546a091c279d52f5e506634
Zeeshan Ahmad <zeeshan949@gmail.com>
2016-04-07 18:47:00 +0500

    initial project creation

---
+++ src/main/java/Restful/Database/Bridge/RestDatabaseBridgeApplication.java
@@ -0,0 +1,12 @@
+package Restful.Database.Bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDatabaseBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDatabaseBridgeApplication.class, args);
+	}
+}
